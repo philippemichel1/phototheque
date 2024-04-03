a0061ced0fe1d5425d8bb6d3b1021b3231011250
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-03 20:07:32 +0200

    Initial Commit

---
+++ photoLibrary/photoLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoLibraryApp.swift
+//  photoLibrary
+//
+//  Created by Philippe MICHEL on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct photoLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
